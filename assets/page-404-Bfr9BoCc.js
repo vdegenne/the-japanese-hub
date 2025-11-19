cd683f9927b939f0be46eed64632171e8a6cdbbd
vdegenne <vdegenne@gmail.com>
2025-11-19 13:34:18 +0000

    Build: (0ce69fb) ignore base path part

---
+++ assets/page-404-Bfr9BoCc.js
@@ -0,0 +1,1 @@
+import{x as p,t as g,w as i,a as m,i as v,s as w}from"./index-BB2bjdaB.js";import{P as O}from"./PageElement-Bk5CxKKH.js";var P=Object.getOwnPropertyDescriptor,f=(t,s,n,a)=>{for(var e=a>1?void 0:a?P(s,n):s,r=t.length-1,o;r>=0;r--)(o=t[r])&&(e=o(e)||e);return e};let l=class extends O{render(){return p`404 NOT FOUND`}};l=f([g("page-404"),i(w),m(v``)],l);export{l as Page404};
